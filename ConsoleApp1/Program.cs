5a8303e4a146cf92867c2450011d4192bb9ed10a
Nick <Npgb18@gmail.com>
2022-12-01 12:44:11 +0000

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,19 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world!");
+
+            bool isTrue = true;
+
+            string isTrueStr = isTrue.ToString();
+
+            for (int i = 0; i < 4; i++)
+            {
+                char x = isTrueStr[i];
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
